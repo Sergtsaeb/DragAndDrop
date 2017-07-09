a5a6c34cf68f6b563c407aacd176f68906fcebe6
Serg T <sergtsaeb@gmail.com>
2017-07-08 21:03:10 -0700

    Split view user interface setup

---
+++ DragAndDrop/FontsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FontsViewController.swift
+//  DragAndDrop
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 7/8/17.
+//  Copyright © 2017 Sergtsaeb. All rights reserved.
+//
+
+import UIKit
+
+class FontsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+  
+
+}
