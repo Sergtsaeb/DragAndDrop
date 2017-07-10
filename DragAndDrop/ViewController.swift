1916a879a9da05456b737ef12aa87d919639512e
Serg T <sergtsaeb@gmail.com>
2017-07-10 14:00:17 -0700

    added color generator function to call in viewDidLoad

--- DragAndDrop/ViewController.swift
+++ DragAndDrop/ViewController.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var postcard: UIImageView!
-    
     @IBOutlet weak var colorSelection: UICollectionView!
-
+    
+    var colors = [UIColor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        self.colorSelection.dataSource = self
-//        self.colorSelection.delegate = self
+        self.colorSelection.dataSource = self
+        self.colorSelection.delegate = self
     }
     
+    func colorGenerator() {
+        
+        colors += [.black, .gray, .white, .yellow, .orange, .red, .magenta, .purple, .blue, .cyan, .green]
+        
+        for i in 0...9 {
+            for j in 1...10 {
+                let color = UIColor(hue: CGFloat(i)/10, saturation: CGFloat(j)/10, brightness: 1, alpha: 1)
+                colors.append(color)
+            }
+            
+        }
+        
+    }
     
-    
-
 
 }
 
